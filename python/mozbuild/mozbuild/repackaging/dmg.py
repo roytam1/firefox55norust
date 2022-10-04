866565e51aa2be56b3122541940dfb3e2256fdec
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:52:22 +0000

    Adding numeric_owner as keyword arguement

--- python/mozbuild/mozbuild/repackaging/dmg.py
+++ python/mozbuild/mozbuild/repackaging/dmg.py
@@ -34,7 +34,7 @@ def repackage_dmg(infile, output):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=tmpdir)
--- taskcluster/taskgraph/util/docker.py
+++ taskcluster/taskgraph/util/docker.py
@@ -157,7 +157,7 @@ def build_from_context(docker_bin, context_path, prefix, tag=None):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tf, d)
